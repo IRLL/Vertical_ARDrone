a6908408bf94ce17112aebbc1f0989534c1bb36d
James Irwin <james.irwin@email.wsu.edu>
2015-05-29 17:26:34 -0700

    started moving to new reset model

--- src/vertical_control/scripts/drone_controller.py
+++ src/vertical_control/scripts/drone_controller.py
@@ -15,55 +15,31 @@ class Controller():
 
 		rospy.init_node('drone_controller', anonymous=False)
 		self.drone_pub = rospy.Publisher('cmd_vel', Twist)
-		self.takeoff_pub = rospy.Publisher('ardrone/takeoff', Empty) 
-		self.reset_pub = rospy.Publisher('v_controller/soft_reset', Empty)
-		self.land_pub = rospy.Publisher('ardrone/land', Empty) 
+		self.reset_sub = rospy.Subscriber('v_controller/soft_reset', Empty, self.reset)
 		self.agent_sub = rospy.Subscriber('v_controller/agent_cmd', Twist, self.rx_agent_callback)
 		self.stabilizer_sub = rospy.Subscriber('v_controller/stabilizer_cmd', Twist, self.rx_stabilizer_callback)
-		self.visible_sub = rospy.Subscriber('v_controller/not_visible', Empty, self.not_visible_callback)
 
 		self.rate = rospy.Rate(self.UPDATE_SPEED)
+
 	def rx_agent_callback(self, data):
 		self.agent_twist = data
 
 	def rx_stabilizer_callback(self, data):
 		self.stabilizer_twist = data
 
-	def not_visible_callback(self, data):
-		print "can't see the ball, resetting!"
-		self.reset()
-	
 	def run(self):
 		cmd = self.stabilizer_twist
 		cmd.linear.z = self.agent_twist.linear.z
 		self.drone_pub.publish(cmd)
 
-	def reset(self):
-		#command the drone to stop
-		controller.drone_pub.publish(Twist())
-		#reset all objects in gazebo
-		self.gazebo_reset()
-		#make sure the drone is flying
-		controller.takeoff_pub.publish(Empty())
-		#send the stop command again
-		controller.drone_pub.publish(Twist())
-
-		#sleep for a bit
-		rospy.sleep(5)
-		
-		#command all other modules to reset themselves
-		controller.reset_pub.publish(Empty())
-		
+	def reset(self, empty):
+		pass	
 
 def land_drone():
 	controller.land_pub.publish(Empty())
 
 if __name__ == "__main__":
 	controller = Controller()
-	rospy.on_shutdown(land_drone)
-	time.sleep(.5)
-	controller.reset()
-	rospy.sleep(0.5)
 
 	while not rospy.is_shutdown():
 		controller.run()
--- src/vertical_control/scripts/main.py
+++ src/vertical_control/scripts/main.py
@@ -1,102 +1,61 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 import rospy
+import std_srvs.srv as services
+from std_msgs.msg import Bool
 
-from sensor_msgs.msg import Image       # for recieving video feed
-from geometry_msgs.msg import Twist     # controlling the movements
-from std_msgs.msg import Empty
-import numpy as np
-import cv2
-from image_converter import ToOpenCV
-from threading import Lock
-from vision_processor import Vision_Processor
-from controller import stabilizer
-
-class Lab5():
+class Main():
 	def __init__(self):
-		self.video_sub = rospy.Subscriber('/ardrone/image_raw',Image,self.Receive_image)
-		self.drone_pub = rospy.Publisher('cmd_vel', Twist)
-		self.drone_land_pub = rospy.Publisher('/ardrone/land', Empty)
-		self.drone_takeoff_pub = rospy.Publisher('/ardrone/takeoff', Empty)
-		rospy.init_node('lab5', anonymous=True)
-		self.vp = Vision_Processor()
-		self.controller = pi_controller_2d(.002, .0001, .05)
-		self.go_forward = 0
-
-
-	def Receive_image(self, data):
-		self.image_lock.acquire()
-		try:
-			self.image = data
-		finally:
-			self.image_lock.release()
-	def center_distance(self, position):
-		screen_center= (self.vp.height/2, self.vp.width/2)
-		distance_x = (position[0] - screen_center[1])
-		distance_y = -1*(position[1] - screen_center[0])
-
-		return distance_x, distance_y
-
-	def main_process(self):
-		key = None
-		command = Twist()
-		class error:
-			x=0
-			y=0
-
-		self.image_lock.acquire()
-		try:
-			temp_image = self.image
-		finally:
-			self.image_lock.release()
-		if temp_image is None:
-			pass
-		else:
-			cv_image = np.asarray(ToOpenCV(temp_image))
-			ball_center = self.vp.process_image(cv_image, (0, 170, 0), (200, 255, 255))
-			if (ball_center == (-1,-1)):
-				print 'looking for ball!'
-				command.angular.z = -.5	
-
-			else:
-				#print "center: %f,%f" % (self.vp.height/2, self.vp.width/2)
-				#print "ball: %f,%f" % (ball_center[0], ball_center[1])
-				error.x, error.y = self.center_distance(ball_center)
-				correction = self.controller.run(error)
-				print correction.x, correction.y
-				command.angular.z = -correction.x
-				command.linear.z = correction.y
-				if (abs(correction.x) < .01):
-					self.go_forward = 1
-				if (self.go_forward == 1):
-					command.linear.x = 1
-					print "going forward!"
-			self.drone_pub.publish(command)
-			#cv2.imshow("raw image", self.vp.images['src'])
-			#cv2.imshow("HSV", self.vp.images['hsv'])
-			#cv2.imshow("color filtered", self.vp.images['color_filter'])
-			#cv2.imshow("contours", self.vp.images['contours'])
-			cv2.imshow("center", self.vp.images['center'])
-			#print ball_center
-			key = cv2.waitKey(50) & 0xFF
-		return key
-
-def main():
-	exit = 0
-	instance = Lab5()
-
-	#start flying!
-	rospy.sleep(.5)
-	instance.drone_takeoff_pub.publish(Empty())
-
-	while(exit == 0):
-
-		key = instance.main_process()
-		if key == ord('q'):
-			exit = 1
-	cv2.destroyAllWindows()
-	instance.drone_land_pub.publish(Empty())
-
-if __name__ == '__main__':
-	main()
+		rospy.init_node('v_control_main')
+		s = rospy.Service('/v_control/reset_world', services.Empty, self.reset_world)
 		
+		self.gazebo_reset = rospy.ServiceProxy('/gazebo/reset_world', services.Empty)
+
+		self.takeoff_pub = rospy.Publisher('ardrone/takeoff', Empty)
+		self.land_pub = rospy.Publisher('ardrone/land', Empty)
+		self.reset_pub = rospy.Publisher('v_controller/soft_reset', Empty)
+		self.enable_pub = rospy.Publisher('v_controller/move_enable', Bool)
+
+
+		
+
+		#wait a bit before publishing
+		time.sleep(.5)
+		self.reset_pub.publish(Empty())
+		self.enable_pub.publish(Bool(1)) #enable the drone controller
+
+		
+
+		#register closing function
+		rospy.on_shutdown(self.exit_handler)
+
+		rospy.spin()
+
+	def exit_handler():
+		controller.land_pub.publish(Empty())
+
+	def reset_world(self, req):
+		print "got reset request"
+		#do all resetting here
+		
+		#disable the drone controller, publish an empty message
+		self.enable_pub.publish(Bool(0)) #disable the drone controller
+		self.drone_pub.publish(Twist()) #publish command to stop drone
+		
+		#reset the world
+		self.gazebo_reset()
+
+
+		self.controller.takeoff_pub.publish(Empty())
+
+		rospy.sleep(5)
+		self.enable_pub.publish(Bool(1)) #enable the drone controller
+		print "done!"
+		return services.EmptyResponse()
+
+
+		
+
+
+if __name__ == "__main__":
+	h = Main()
